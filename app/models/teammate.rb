4eebbbef094dafb69bc130c3ad362cd81f0678f9
Van Michael <van.michael.nguyen@gmail.com>
2014-01-14 15:03:31 -0500

    refactor and clean up

--- app/models/teammate.rb
+++ app/models/teammate.rb
@@ -9,18 +9,11 @@ class Teammate < ActiveRecord::Base
 
   class << self
     def add_teammate(current_user,teammate)
-      if person = Person.find_or_initialize_by(email: teammate[:person][:email])
-         person.name = teammate[:person][:name]
-         person.save
-         teammate = Teammate.new(account_id: current_user.id, person_id: person.id)
-        if teammate.save
-          return true
-        else
-          return false
-        end
-      else
-        return false
-      end
+     person = Person.find_or_initialize_by(email: teammate[:person][:email])
+     person.name = teammate[:person][:name]
+     person.save
+     teammate = Teammate.new(account_id: current_user.id, person_id: person.id)
+     teammate.save
     end
   end
 end
