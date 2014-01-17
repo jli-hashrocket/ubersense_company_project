7c50e03d9eca30fa98ee759c72b251cbd9827820
Van Michael <van.michael.nguyen@gmail.com>
2014-01-17 02:52:18 -0500

    add styling

---
+++ test/integration/coach_imports_csv_test.rb
@@ -0,0 +1,48 @@
+require 'test_helper'
+
+class CoachImportsCsvTest < ActionDispatch::IntegrationTest
+  def setup
+    Capybara.reset_sessions!
+  end
+
+  test 'should import valid csv' do
+    coach = FactoryGirl.create(:account)
+    sign_in_as(coach.email, 'password')
+
+    page.find("#file").click
+    attach_file "file", Rails.root.to_s + '/test/sample_csv/sample.csv'
+    click_button("Import CSV")
+
+    assert page.has_content?('CSV uploaded, and teammates added!')
+    assert page.has_content?('test@gmail.com')
+  end
+
+  test 'should not import invalid file type' do
+    coach = FactoryGirl.create(:account)
+    sign_in_as(coach.email, 'password')
+
+    page.find("#file").click
+    attach_file "file", Rails.root.to_s + '/test/sample_csv/sample.rtf'
+    click_button("Import CSV")
+
+    assert page.has_content?("Sorry invalid file type or wrong headers!")
+  end
+
+  test 'should not import invalid headers' do
+    coach = FactoryGirl.create(:account)
+    sign_in_as(coach.email, 'password')
+
+    page.find("#file").click
+    attach_file "file", Rails.root.to_s + '/test/sample_csv/sample.rtf'
+    click_button("Import CSV")
+
+    assert page.has_content?("Sorry invalid file type or wrong headers!")
+  end
+
+  # test 'not import duplicate teammates'
+
+  # test 'updates name if duplicate email'
+
+  # test 'submit form without adding CSV'
+
+end
